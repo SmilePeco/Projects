c12779c73989e7c104814470bb5396c143b3ea94
SmilePeco <63031036+SmilePeco@users.noreply.github.com>
2020-04-13 10:46:19 +0900

    INIファイル実装前

---
+++ T004/T004/T004.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class T004
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnExcelSave = New System.Windows.Forms.Button()
+        Me.btnEnd = New System.Windows.Forms.Button()
+        Me.btn = New System.Windows.Forms.Button()
+        Me.dtpDateTo = New System.Windows.Forms.DateTimePicker()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.dtpDateFrom = New System.Windows.Forms.DateTimePicker()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtItemName = New System.Windows.Forms.TextBox()
+        Me.btnIemNoSearch = New System.Windows.Forms.Button()
+        Me.txtItemNO = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.btnExcelPrint = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnExcelPrint)
+        Me.GroupBox1.Controls.Add(Me.btnExcelSave)
+        Me.GroupBox1.Controls.Add(Me.btnEnd)
+        Me.GroupBox1.Controls.Add(Me.btn)
+        Me.GroupBox1.Controls.Add(Me.dtpDateTo)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.dtpDateFrom)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtItemName)
+        Me.GroupBox1.Controls.Add(Me.btnIemNoSearch)
+        Me.GroupBox1.Controls.Add(Me.txtItemNO)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(811, 97)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'btnExcelSave
+        '
+        Me.btnExcelSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExcelSave.Location = New System.Drawing.Point(454, 13)
+        Me.btnExcelSave.Name = "btnExcelSave"
+        Me.btnExcelSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnExcelSave.TabIndex = 29
+        Me.btnExcelSave.Text = "保存"
+        Me.btnExcelSave.UseVisualStyleBackColor = True
+        '
+        'btnEnd
+        '
+        Me.btnEnd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnEnd.Location = New System.Drawing.Point(727, 12)
+        Me.btnEnd.Name = "btnEnd"
+        Me.btnEnd.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnd.TabIndex = 28
+        Me.btnEnd.Text = "終了"
+        Me.btnEnd.UseVisualStyleBackColor = True
+        '
+        'btn
+        '
+        Me.btn.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btn.Location = New System.Drawing.Point(631, 13)
+        Me.btn.Name = "btn"
+        Me.btn.Size = New System.Drawing.Size(75, 23)
+        Me.btn.TabIndex = 27
+        Me.btn.Text = "クリア"
+        Me.btn.UseVisualStyleBackColor = True
+        '
+        'dtpDateTo
+        '
+        Me.dtpDateTo.Location = New System.Drawing.Point(243, 19)
+        Me.dtpDateTo.Name = "dtpDateTo"
+        Me.dtpDateTo.Size = New System.Drawing.Size(127, 19)
+        Me.dtpDateTo.TabIndex = 24
+        Me.dtpDateTo.Value = New Date(2020, 4, 11, 9, 21, 0, 0)
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(220, 24)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(17, 12)
+        Me.Label6.TabIndex = 26
+        Me.Label6.Text = "～"
+        '
+        'dtpDateFrom
+        '
+        Me.dtpDateFrom.Location = New System.Drawing.Point(87, 19)
+        Me.dtpDateFrom.Name = "dtpDateFrom"
+        Me.dtpDateFrom.Size = New System.Drawing.Size(127, 19)
+        Me.dtpDateFrom.TabIndex = 23
+        Me.dtpDateFrom.Value = New Date(2020, 4, 11, 9, 21, 0, 0)
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 24)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 12)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Text = "日付"
+        '
+        'txtItemName
+        '
+        Me.txtItemName.Location = New System.Drawing.Point(153, 55)
+        Me.txtItemName.Name = "txtItemName"
+        Me.txtItemName.Size = New System.Drawing.Size(154, 19)
+        Me.txtItemName.TabIndex = 22
+        Me.txtItemName.Text = "txtWorkProcessName"
+        '
+        'btnIemNoSearch
+        '
+        Me.btnIemNoSearch.Location = New System.Drawing.Point(124, 53)
+        Me.btnIemNoSearch.Name = "btnIemNoSearch"
+        Me.btnIemNoSearch.Size = New System.Drawing.Size(28, 23)
+        Me.btnIemNoSearch.TabIndex = 21
+        Me.btnIemNoSearch.Text = "..."
+        Me.btnIemNoSearch.UseVisualStyleBackColor = True
+        '
+        'txtItemNO
+        '
+        Me.txtItemNO.Location = New System.Drawing.Point(87, 55)
+        Me.txtItemNO.MaxLength = 3
+        Me.txtItemNO.Name = "txtItemNO"
+        Me.txtItemNO.Size = New System.Drawing.Size(37, 19)
+        Me.txtItemNO.TabIndex = 19
+        Me.txtItemNO.Text = "999"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 12)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "製品NO"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.GroupBox5)
+        Me.GroupBox3.Controls.Add(Me.GroupBox4)
+        Me.GroupBox3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.GroupBox3.Location = New System.Drawing.Point(0, 97)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(811, 281)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.Chart1)
+        Me.GroupBox5.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox5.Location = New System.Drawing.Point(430, 15)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(378, 263)
+        Me.GroupBox5.TabIndex = 1
+        Me.GroupBox5.TabStop = False
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.DataGridView1)
+        Me.GroupBox4.Dock = System.Windows.Forms.DockStyle.Left
+        Me.GroupBox4.Location = New System.Drawing.Point(3, 15)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(427, 263)
+        Me.GroupBox4.TabIndex = 0
+        Me.GroupBox4.TabStop = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 15)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 21
+        Me.DataGridView1.Size = New System.Drawing.Size(421, 245)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Chart1
+        '
+        ChartArea2.AxisX.Interval = 1.0R
+        ChartArea2.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea2)
+        Me.Chart1.Dock = System.Windows.Forms.DockStyle.Fill
+        Legend2.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend2)
+        Me.Chart1.Location = New System.Drawing.Point(3, 15)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Size = New System.Drawing.Size(372, 245)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'btnExcelPrint
+        '
+        Me.btnExcelPrint.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExcelPrint.Location = New System.Drawing.Point(535, 12)
+        Me.btnExcelPrint.Name = "btnExcelPrint"
+        Me.btnExcelPrint.Size = New System.Drawing.Size(75, 23)
+        Me.btnExcelPrint.TabIndex = 30
+        Me.btnExcelPrint.Text = "印刷"
+        Me.btnExcelPrint.UseVisualStyleBackColor = True
+        '
+        'T004
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(811, 390)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "T004"
+        Me.Text = "T004_製品程別理論在庫計算"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtItemName As System.Windows.Forms.TextBox
+    Friend WithEvents btnIemNoSearch As System.Windows.Forms.Button
+    Friend WithEvents txtItemNO As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents dtpDateTo As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents dtpDateFrom As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnExcelSave As System.Windows.Forms.Button
+    Friend WithEvents btnEnd As System.Windows.Forms.Button
+    Friend WithEvents btn As System.Windows.Forms.Button
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox5 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Chart1 As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents btnExcelPrint As System.Windows.Forms.Button
+
+End Class
---
+++ T004/T004/T004.vb
@@ -0,0 +1,439 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Office.Interop
+Imports System.Runtime.InteropServices
+
+
+Public Class T004
+
+    Dim strConnect As String 'DB接続用
+    Dim Cn As New System.Data.SqlClient.SqlConnection
+    Dim cd As System.Data.SqlClient.SqlCommand
+    Dim strSQL As String
+    Dim strServerName As String = "SHINYA-PC\NAKADB" 'サーバー名
+    Dim strUserID As String = "sa" 'ユーザーID
+    Dim strPassword As String = "naka" 'パスワード
+    Dim strDatabaseName As String = "NAKADB" 'データベース
+    Dim blnResult As Boolean '名前・パスワード判定用
+
+    '---------------------------------------------
+    '---ロード処理                             ---
+    '---------------------------------------------
+    Private Sub T004_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Call sClear()
+
+    End Sub
+
+    '---------------------------------------------
+    '---製品NO KeyPress処理                     ---
+    '---------------------------------------------
+    Private Sub txtItemNO_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtItemNO.KeyPress
+        '0～9と、バックスペース以外の時は、イベントをキャンセルする
+        If (e.KeyChar < "0"c OrElse "9"c < e.KeyChar) AndAlso _
+                e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+    '---------------------------------------------
+    '---製品NO KeyDown処理                     ---
+    '---------------------------------------------
+    Private Sub txtItemNO_KeyDown(sender As Object, e As KeyEventArgs) Handles txtItemNO.KeyDown
+
+        If e.KeyData = Keys.Enter Then
+            '３桁まで０埋め
+            txtItemNO.Text = txtItemNO.Text.PadLeft(3, "0")
+
+            'DB接続
+            Call sDBConnect()
+
+            'DataGridViewの値取得
+            Call sDataGridViewConnect()
+
+
+
+
+        End If
+
+    End Sub
+
+    '------------------------------------------------
+    '--保存ボタンの押下                    ----------
+    '------------------------------------------------
+    Private Sub btnExcelSave_Click(sender As Object, e As EventArgs) Handles btnExcelSave.Click
+
+        Call sExcelSavePrint(1)
+
+    End Sub
+
+    '------------------------------------------------
+    '--印刷ボタンの押下                    ----------
+    '------------------------------------------------
+    Private Sub btnExcelPrint_Click(sender As Object, e As EventArgs) Handles btnExcelPrint.Click
+
+        Call sExcelSavePrint(2)
+
+    End Sub
+
+    '------------------------------------------------
+    '--クリアボタンの押下                    ----------
+    '------------------------------------------------
+    Private Sub btn_Click(sender As Object, e As EventArgs) Handles btn.Click
+        sClear()
+    End Sub
+
+    '------------------------------------------------
+    '--終了ボタンの押下                    ----------
+    '------------------------------------------------
+    Private Sub btnEnd_Click(sender As Object, e As EventArgs) Handles btnEnd.Click
+        Me.Close()
+    End Sub
+
+    '------------------------------------------------
+    '--SQLServerへの接続の開始             ----------
+    '------------------------------------------------
+    Public Sub sDBConnect()
+
+        'DBに接続
+        strServerName = "SHINYA-PC\NAKADB" 'サーバー名(またはIPアドレス)
+        strUserID = "sa" 'ユーザーID
+        strPassword = "naka" 'パスワード
+        strDatabaseName = "NAKADB" 'データベース
+
+        strConnect = "Server=" & strServerName & ";"
+        strConnect &= "User ID=" & strUserID & ";"
+        strConnect &= "Password=" & strPassword & ";"
+        strConnect &= "Initial Catalog=" & strDatabaseName
+
+        Cn.ConnectionString = strConnect
+
+        cd = Cn.CreateCommand
+
+        Cn.Open()
+
+    End Sub
+
+    '---------------------------------------------
+    '---クリア処理                             ---
+    '---------------------------------------------
+    Public Sub sClear()
+
+        dtpDateFrom.Text = Now
+        dtpDateTo.Text = Now.AddDays(5)
+
+        txtItemNO.Text = ""
+        txtItemName.Text = ""
+        txtItemName.Enabled = False
+
+        DataGridView1.DataSource = Nothing
+        DataGridView1.Columns.Clear()
+
+
+        Chart1.Series.Clear()
+
+    End Sub
+
+    '---------------------------------------------
+    '---DataGridView表示処理                   ---
+    '---------------------------------------------
+    Public Function sDataGridViewConnect() As Boolean
+
+        Dim dtReader As SqlDataReader
+
+        Try
+
+            '製品NOが存在するのか確認
+            strSQL = ""
+            strSQL &= "SELECT "
+            strSQL &= " ""製品NO"" "
+            strSQL &= "FROM "
+            strSQL &= " ITEM_MS "
+            strSQL &= "WHERE "
+            strSQL &= "  ""製品NO"" = '" + txtItemNO.Text.Trim + "' "
+
+            cd.CommandText = strSQL
+            cd.Connection = Cn
+            cd.ExecuteNonQuery()
+            dtReader = cd.ExecuteReader()
+
+            If dtReader.Read = True Then
+
+                dtReader.Close()
+
+                '出荷、生産があるか確認
+                strSQL = ""
+                strSQL &= "SELECT "
+                strSQL &= "  A.""出荷日"", "
+                strSQL &= "  A.""出荷数"", "
+                strSQL &= "  B.""生産日"", "
+                strSQL &= "  B.""生産数"" "
+                strSQL &= " FROM "
+                strSQL &= "  SHIPMENT_TBL A, "
+                strSQL &= "  CREATE_TBL B "
+                strSQL &= " WHERE "
+                strSQL &= "(    A.""出荷日"" BETWEEN '" + dtpDateFrom.Value.Date + "' AND '" + dtpDateTo.Value.Date + "' "
+                strSQL &= "OR B.""生産日"" BETWEEN '" + dtpDateFrom.Value.Date + "' AND '" + dtpDateTo.Value.Date + "') "
+
+                cd.CommandText = strSQL
+                cd.Connection = Cn
+                cd.ExecuteNonQuery()
+                dtReader = cd.ExecuteReader()
+
+                If dtReader.Read = True Then
+
+                    dtReader.Close()
+
+                    strSQL = ""
+                    strSQL &= "SELECT "
+                    strSQL &= " CASE "
+                    strSQL &= "  WHEN C.""出荷日"" is null THEN B.""生産日"" "
+                    strSQL &= "  WHEN B.""生産日"" is null THEN C.""出荷日"" "
+                    strSQL &= "  ELSE B.""生産日"" "
+                    strSQL &= " END ""日付"", "
+                    strSQL &= " B.""生産数"", "
+                    strSQL &= " C.""出荷数"" "
+                    strSQL &= "FROM "
+                    strSQL &= " (SELECT "
+                    strSQL &= "   A.""製品NO"", "
+                    strSQL &= "   A.""製品名"", "
+                    strSQL &= "   B.""作業工程NO"" "
+                    strSQL &= "  FROM "
+                    strSQL &= "   ITEM_MS A, "
+                    strSQL &= "   WORKPROCESS_MS B "
+                    strSQL &= "  WHERE "
+                    strSQL &= "   A.""製品NO"" = '" + txtItemNO.Text.Trim + "' "
+                    strSQL &= "   AND A.""製品NO"" = B.""製品NO"") A, "
+                    strSQL &= " (SELECT "
+                    strSQL &= "   ""作業工程NO"", "
+                    strSQL &= "   ""生産数"", "
+                    strSQL &= "   ""生産日"" "
+                    strSQL &= "FROM "
+                    strSQL &= "     CREATE_TBL "
+                    strSQL &= ") B "
+                    strSQL &= " FULL OUTER JOIN "
+                    strSQL &= " (SELECT "
+                    strSQL &= "   ""作業工程NO"", "
+                    strSQL &= "   ""出荷数"", "
+                    strSQL &= "   ""出荷日"" "
+                    strSQL &= "FROM "
+                    strSQL &= "   SHIPMENT_TBL "
+                    strSQL &= ") C ON B.""生産日"" = C.""出荷日"" "
+                    strSQL &= " AND C.""出荷日"" = B.""生産日"" "
+                    strSQL &= "WHERE "
+                    strSQL &= " (A.""作業工程NO"" = B.""作業工程NO"" "
+                    strSQL &= "OR A.""作業工程NO"" = C.""作業工程NO"") "
+                    strSQL &= "AND (B.""生産日"" BETWEEN '" + dtpDateFrom.Value.Date + "' AND '" + dtpDateTo.Value.Date + "'  "
+                    strSQL &= "OR   C.""出荷日"" BETWEEN '" + dtpDateFrom.Value.Date + "' AND '" + dtpDateTo.Value.Date + "') "
+                    strSQL &= " ORDER BY "
+                    strSQL &= " ""日付"" "
+
+                    Dim dsDataSet As New DataSet
+                    Dim daDataAdapter As New SqlClient.SqlDataAdapter
+
+                    daDataAdapter.SelectCommand = New SqlClient.SqlCommand(strSQL, Cn)
+                    daDataAdapter.SelectCommand.CommandTimeout = 0
+                    daDataAdapter.Fill(dsDataSet, "TABLE001")
+                    DataGridView1.DataSource = dsDataSet.Tables("TABLE001")
+
+                    'グラフの初期化
+                    Chart1.Series.Clear()
+
+                    'グラフの設定
+                    With Chart1
+                        'データソースをつなげる
+                        .DataSource = dsDataSet
+                        '末尾まで繰り返す
+                        For i = 1 To dsDataSet.Tables("TABLE001").Columns.Count - 1
+                            Dim strColumnName As String = dsDataSet.Tables("TABLE001").Columns(i).ColumnName.ToString()
+                            .Series.Add(strColumnName)
+                            'X 軸のラベルテキストの読込・設定(日付の設定)
+                            .Series(strColumnName).XValueMember = dsDataSet.Tables("TABLE001").Columns(0).ColumnName.ToString()
+                            'Y 軸のラベルテキストの読込・設定(生産数、出荷数の設定)
+                            .Series(strColumnName).YValueMembers = strColumnName
+
+                        Next
+
+                        'Chart1.ChartAreas(0).AxisX.Maximum = dsDataSet.Tables(0).Columns.Count - 1
+
+                        'データポイントラベルの設定
+                        .Series(0).Label = "#VALY"
+                        .Series(1).Label = "#VALY"
+
+                    End With
+
+
+                    DataGridView1.Columns(1).DefaultCellStyle.Format = "G"
+                    DataGridView1.Columns(1).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+                    DataGridView1.Columns(2).DefaultCellStyle.Format = "G"
+                    DataGridView1.Columns(2).DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight
+
+                    Cn.Close()
+                    Cn.Dispose()
+
+                    Return True
+
+
+
+                Else
+                    MessageBox.Show("出荷、もしくは生産の実績がありません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    Cn.Close()
+                    Cn.Dispose()
+
+                    Return False
+
+                End If
+
+            Else
+                MessageBox.Show("その製品NOは存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Cn.Close()
+                Cn.Dispose()
+
+                Return False
+
+
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString, "例外発生")
+            Return False
+
+
+        Finally
+            Cn.Close()
+            Cn.Dispose()
+
+        End Try
+
+        '製品NOが登録されているか確認
+
+        '日付で検索して一致するか
+
+        '一致したら取得
+
+    End Function
+
+    '---------------------------------------------
+    '---DataGridView表示処理                   ---
+    '---------------------------------------------
+    Public Sub sExcelSavePrint(intDec As Integer)
+
+        'DataGridViewが０件の場合はエラー
+        If DataGridView1.Columns.Count <> 0 Then
+            ' EXCEL関連オブジェクトの定義
+            Dim objExcel As Excel.Application = New Excel.Application
+            Dim objWorkBook As Excel.Workbook = objExcel.Workbooks.Add
+            'Dim objSheet As Excel.Worksheet = Nothing
+
+
+
+            Dim sfd As New SaveFileDialog()
+
+            '初期設定
+            sfd.InitialDirectory = "C:\"
+            sfd.Filter = "EXCELファイル(*.xlsx)|*.xlsx"
+            sfd.Title = "保存先のファイルを選択してください。"
+
+
+
+            'シートの最大表示列項目数
+            Dim columnMaxNum As Integer = DataGridView1.Columns.Count - 1
+            'シートの最大表示行項目数
+            Dim rowMaxNum As Integer = DataGridView1.Rows.Count - 1
+
+            '項目名格納用リストを宣言
+            Dim columnList As New List(Of String)
+            'ヘッダー名を取得
+            For i As Integer = 0 To (columnMaxNum)
+                columnList.Add(DataGridView1.Columns(i).HeaderCell.Value)
+            Next
+
+            'セルのデータ取得用二次元配列を宣言
+            Dim v As String(,) = New String(rowMaxNum, columnMaxNum) {}
+
+            For row As Integer = 0 To rowMaxNum
+                For col As Integer = 0 To columnMaxNum
+                    If DataGridView1.Rows(row).Cells(col).Value Is Nothing = False Then
+                        ' セルに値が入っている場合、二次元配列に格納
+                        '1列目：文字列型、2列目：数値、3列目：数値
+                        If col = 0 Then
+                            '文字列型("YYYY-MM-DD")
+                            v(row, col) = Strings.Left(DataGridView1.Rows(row).Cells(col).Value.ToString(), 10)
+                        ElseIf col = 1 Or col = 2 Then
+                            '数値型
+                            If IsDBNull(DataGridView1.Rows(row).Cells(col).Value) = False Then
+                                'v(row, col) = CInt(DataGridView1.Rows(row).Cells(col).Value)
+                                v(row, col) = DataGridView1.Rows(row).Cells(col).Value
+                            Else
+                                v(row, col) = 0
+
+                            End If
+
+                        End If
+
+                    End If
+                Next
+            Next
+
+            ' EXCELに項目名を転送
+            For i As Integer = 1 To DataGridView1.Columns.Count
+                ' シートの一行目に項目を挿入
+                objWorkBook.Sheets(1).Cells(1, i) = columnList(i - 1)
+
+                ' 罫線を設定
+                objWorkBook.Sheets(1).Cells(1, i).Borders.LineStyle = True
+                '列幅の調整
+                objWorkBook.Sheets(1).Columns("A").ColumnWidth = 12
+                ' 項目の表示行に背景色を設定
+                objWorkBook.Sheets(1).Cells(1, i).Interior.Color = RGB(140, 140, 140)
+                ' 文字のフォントを設定
+                objWorkBook.Sheets(1).Cells(1, i).Font.Color = RGB(255, 255, 255)
+                objWorkBook.Sheets(1).Cells(1, i).Font.Bold = True
+            Next
+
+            ' EXCELにデータを範囲指定で転送
+            Dim strRange As String = "A2:" & Chr(Asc("A") + columnMaxNum) & DataGridView1.Rows.Count + 1
+            objWorkBook.Sheets(1).Range(strRange) = v
+            ' データの表示範囲に罫線を設定
+            objWorkBook.Sheets(1).Range(strRange).Borders.LineStyle = True
+
+
+
+
+
+            '保存か印刷判定
+            If intDec = 1 Then
+                '保存の場合
+                If sfd.ShowDialog() = DialogResult.OK Then
+                    objWorkBook.SaveAs(Filename:=sfd.FileName)
+                    MessageBox.Show("保存できました。", "情報", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+                End If
+            ElseIf intDec = 2 Then
+                '印刷の場合
+                ' エクセル表示
+                objExcel.Visible = True
+                objExcel.DisplayAlerts = False
+
+                objWorkBook.Sheets(1).PrintPreview()
+
+
+            End If
+
+            'Marshal.ReleaseComObject(objWorkBook)
+            'Marshal.ReleaseComObject(objExcel)
+
+            objWorkBook.Close()
+            objExcel.Quit()
+
+            objWorkBook = Nothing
+            objExcel = Nothing
+
+        Else
+            MessageBox.Show("検索結果が０件なので保存できません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+
+        End If
+
+    End Sub
+
+
+End Class
