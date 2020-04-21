3b2f9a0d47d1022c5a137847ac943a3333661464
SmilePeco <63031036+SmilePeco@users.noreply.github.com>
2020-04-21 15:13:39 +0900

    作業途中Commit.

---
+++ T002/T002/T002.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class T002
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtWorkNo = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtWorkName = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnEntry = New System.Windows.Forms.Button()
+        Me.btnEnd = New System.Windows.Forms.Button()
+        Me.lblMode = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "作業NO"
+        '
+        'txtWorkNo
+        '
+        Me.txtWorkNo.Location = New System.Drawing.Point(84, 21)
+        Me.txtWorkNo.Name = "txtWorkNo"
+        Me.txtWorkNo.Size = New System.Drawing.Size(100, 19)
+        Me.txtWorkNo.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "作業名"
+        '
+        'txtWorkName
+        '
+        Me.txtWorkName.Location = New System.Drawing.Point(84, 65)
+        Me.txtWorkName.Name = "txtWorkName"
+        Me.txtWorkName.Size = New System.Drawing.Size(100, 19)
+        Me.txtWorkName.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblMode)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtWorkName)
+        Me.GroupBox1.Controls.Add(Me.txtWorkNo)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(513, 157)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnSearch)
+        Me.GroupBox2.Controls.Add(Me.btnCancel)
+        Me.GroupBox2.Controls.Add(Me.btnDelete)
+        Me.GroupBox2.Controls.Add(Me.btnEntry)
+        Me.GroupBox2.Controls.Add(Me.btnEnd)
+        Me.GroupBox2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.GroupBox2.Location = New System.Drawing.Point(0, 107)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(513, 50)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(6, 18)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(90, 23)
+        Me.btnSearch.TabIndex = 13
+        Me.btnSearch.Text = "F1:検索"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(298, 18)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(90, 23)
+        Me.btnCancel.TabIndex = 16
+        Me.btnCancel.Text = "F11:キャンセル"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(202, 18)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(90, 23)
+        Me.btnDelete.TabIndex = 15
+        Me.btnDelete.Text = "F6:削除"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnEntry
+        '
+        Me.btnEntry.Location = New System.Drawing.Point(104, 18)
+        Me.btnEntry.Name = "btnEntry"
+        Me.btnEntry.Size = New System.Drawing.Size(90, 23)
+        Me.btnEntry.TabIndex = 14
+        Me.btnEntry.Text = "F5:登録"
+        Me.btnEntry.UseVisualStyleBackColor = True
+        '
+        'btnEnd
+        '
+        Me.btnEnd.Location = New System.Drawing.Point(403, 18)
+        Me.btnEnd.Name = "btnEnd"
+        Me.btnEnd.Size = New System.Drawing.Size(90, 23)
+        Me.btnEnd.TabIndex = 17
+        Me.btnEnd.Text = "F12:終了"
+        Me.btnEnd.UseVisualStyleBackColor = True
+        '
+        'lblMode
+        '
+        Me.lblMode.AutoSize = True
+        Me.lblMode.Font = New System.Drawing.Font("MS UI Gothic", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.lblMode.Location = New System.Drawing.Point(200, 92)
+        Me.lblMode.Name = "lblMode"
+        Me.lblMode.Size = New System.Drawing.Size(51, 12)
+        Me.lblMode.TabIndex = 4
+        Me.lblMode.Text = "lblMode"
+        '
+        'T002
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(513, 157)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "T002"
+        Me.Text = "T002_作業マスタメンテナンス"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtWorkNo As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtWorkName As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents btnEntry As System.Windows.Forms.Button
+    Friend WithEvents btnEnd As System.Windows.Forms.Button
+    Friend WithEvents lblMode As System.Windows.Forms.Label
+
+End Class
---
+++ T002/T002/T002.vb
@@ -0,0 +1,243 @@
+﻿Imports System.Data.SqlClient
+
+Public Class T002
+
+    Dim strConnect As String 'DB接続用
+    Dim Cn As New System.Data.SqlClient.SqlConnection
+    Dim cd As System.Data.SqlClient.SqlCommand
+    Dim strSQL As String
+    Dim strServerName As String = "SHINYA-PC\NAKADB" 'サーバー名
+    Dim strUserID As String = "sa" 'ユーザーID
+    Dim strPassword As String = "naka" 'パスワード
+    Dim strDatabaseName As String = "NAKADB" 'データベース
+    Dim blnResult As Boolean '名前・パスワード判定用
+
+    '------------------------------------------------
+    '--Load処理                            ----------
+    '------------------------------------------------
+    Private Sub T002_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        sClear()
+
+    End Sub
+
+    '------------------------------------------------
+    '--ファンクションキー操作処理          ----------
+    '------------------------------------------------
+    Private Sub T002_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+        Select Case e.KeyCode
+
+            Case Keys.F1
+                If btnSearch.Enabled = True Then
+                    '//「F1検索」押下
+                    '検索処理
+                    Call frmSearch()
+                End If
+
+            Case Keys.F5
+                If btnEntry.Enabled = True Then
+                    '//「F5登録」押下
+                    '新規登録・更新処理
+                    Call frmEntry()
+                End If
+
+            Case Keys.F6
+                If btnDelete.Enabled = True Then
+                    '//「F6削除」押下
+                    '削除処理
+                    Call frmDelete()
+                End If
+
+            Case Keys.F11
+                '//「F11キャンセル」押下
+                'クリア処理
+                Call sClear()
+
+            Case Keys.F12
+                '//「F12終了」押下
+                'クローズ処理
+                Me.Close()
+
+        End Select
+    End Sub
+
+
+
+    '------------------------------------------------
+    '--F1検索ボタン押下処理                ----------
+    '------------------------------------------------
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+
+        '検索処理
+        Call frmSearch()
+
+    End Sub
+
+    '------------------------------------------------
+    '--F5新規登録・更新ボタン押下処理      ----------
+    '------------------------------------------------
+    Private Sub btnEntry_Click(sender As Object, e As EventArgs) Handles btnEntry.Click
+
+        '新規登録・更新処理
+        Call frmEntry()
+
+    End Sub
+
+    '------------------------------------------------
+    '--F6削除ボタン押下処理                ----------
+    '------------------------------------------------
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        '削除処理
+        Call frmDelete()
+
+    End Sub
+
+    '------------------------------------------------
+    '--F11キャンセルボタン押下処理         ----------
+    '------------------------------------------------
+    Private Sub blnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+
+        Call sClear()
+
+    End Sub
+
+    '------------------------------------------------
+    '--F12終了ボタン押下処理               ----------
+    '------------------------------------------------
+    Private Sub blnEnd_Click(sender As Object, e As EventArgs) Handles btnEnd.Click
+
+        'クローズ処理
+        Me.Close()
+
+    End Sub
+
+    '------------------------------------------------
+    '--作業No KeyPress処理                 ----------
+    '------------------------------------------------
+    Private Sub txtWorkNo_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtWorkNo.KeyPress
+
+        '入力文字制御(数値のみ)
+        If e.KeyChar < "0"c OrElse "9"c < e.KeyChar Then
+            '押されたキーが 0～9でない場合は、イベントをキャンセルする
+            e.Handled = True
+        End If
+
+    End Sub
+
+    '------------------------------------------------
+    '--検索処理                            ----------
+    '------------------------------------------------
+    Public Sub frmSearch()
+
+        'DB接続処理
+        Call sDBConnect()
+        '検索メイン処理()
+        Call frmMainSearch()
+
+    End Sub
+
+    '------------------------------------------------
+    '--DB接続の開始                        ----------
+    '------------------------------------------------
+    Public Sub sDBConnect()
+
+        'DBに接続
+        strServerName = "SHINYA-PC\NAKADB" 'サーバー名(またはIPアドレス)
+        strUserID = "sa" 'ユーザーID
+        strPassword = "naka" 'パスワード
+        strDatabaseName = "NAKADB" 'データベース
+
+        strConnect = "Server=" & strServerName & ";"
+        strConnect &= "User ID=" & strUserID & ";"
+        strConnect &= "Password=" & strPassword & ";"
+        strConnect &= "Initial Catalog=" & strDatabaseName
+
+        Cn.ConnectionString = strConnect
+
+        cd = Cn.CreateCommand
+
+        Cn.Open()
+
+    End Sub
+
+
+    '------------------------------------------------
+    '--検索メイン処理                      ----------
+    '------------------------------------------------
+    Public Function frmMainSearch() As Boolean
+
+        Dim dtReader As SqlDataReader
+
+        If txtWorkNo.Text.Trim <> "" Then
+
+            Try
+                strSQL = ""
+                strSQL &= "SELECT "
+                strSQL &= "作業No, "
+                strSQL &= "作業NO "
+                strSQL &= "FROM "
+                strSQL &= "WORK_MS "
+                strSQL &= "WHERE "
+                strSQL &= "    社員No= '" + txtWorkNo.Text.ToString("000") + "' "
+                'SQLコマンド設定
+                cd.CommandText = strSQL
+                cd.Connection = Cn
+                cd.ExecuteNonQuery()
+
+                dtReader = cd.ExecuteReader()
+
+                If dtReader.Read() = True Then
+                    lblMode.Text = "更新"
+                    btnDelete.Enabled = True
+                    Call sgrpHumanOpen()
+                    txtName.Text = dtReader("名前").ToString.Trim()
+                    If dtReader("管理者フラグ") = 0 Then
+                        chkAdminFLG.Checked = False
+                    ElseIf dtReader("管理者フラグ") = 1 Then
+                        chkAdminFLG.Checked = True
+                    End If
+                    Return True
+                Else
+
+                    lblMode.Text = "新規作成"
+
+                    Call sgrpHumanOpen()
+                    Cn.Close()
+                    Cn.Dispose()
+
+                    Return True
+
+                End If
+
+            Catch ex As Exception
+                MessageBox.Show(ex.ToString, "例外発生")
+                Return False
+
+            Finally
+                Cn.Close()
+                Cn.Dispose()
+                dtReader.Close()
+
+            End Try
+
+        Else
+            Cn.Close()
+            Cn.Dispose()
+
+            Return False
+        End If
+
+    End Function
+
+    '------------------------------------------------
+    '--フォーム初期値に戻す                ----------
+    '------------------------------------------------
+    Private Sub sClear()
+
+        txtWorkNo.Clear()
+        txtWorkName.Clear()
+
+    End Sub
+End Class
